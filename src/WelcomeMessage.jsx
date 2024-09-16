5c6098deb7e97b6cc6ef434c0194172c0ca0782d
tonyeth <antonio.quistelli@gmail.com>
2024-09-16 10:09:47 +0200

    First App, 1st step

---
+++ src/WelcomeMessage.jsx
@@ -0,0 +1,7 @@
+// src/WelcomeMessage.jsx
+function WelcomeMessage(props) {
+    return <h1 style={{ color: props.color }}>Welcome, {props.name}!</h1>;
+  }
+  
+  export default WelcomeMessage;
+  
